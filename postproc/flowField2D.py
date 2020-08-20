3fc5d741d4a11a9ca942aabe63ce32f6ad156ab4
Francisco Castillo <fjcasti1@asu.edu>
2020-08-19 17:50:24 -0700

    Add Problem & Method inputs. Fix indentation

--- postproc/flowField2D.py
+++ postproc/flowField2D.py
@@ -9,65 +9,69 @@ from myPlots import * # It also imports pylab
 from myReader import * # It also imports numpy
 
 def get_args():
-    class myFormatter(argparse.RawDescriptionHelpFormatter,
-            argparse.ArgumentDefaultsHelpFormatter):
-            pass
-    parser = argparse.ArgumentParser(description=main.__doc__,
-            formatter_class=myFormatter)
-    parser.add_argument('DataFilePath',
-            help='Path to input file, restart or vtk')
-    parser.add_argument('Fields',
-            help='List of fields to plot')
-    parser.add_argument('Clips',type=list,
-            help='List of clipping values for each field')
-    parser.add_argument('-s','--size',dest='figBaseSize', type=int,
-            help='Base size of the figure',default=4)
-    parser.add_argument('-g','--gamma-disable',dest='gammaOpt',action='store_false',
-            help='Aspect ratio of figure: Height/Lenth',default=True)
-    parser.add_argument('-m','--max',dest='gma', type=float,
-            help='Global absolute maximum. gma = max(abs(f)) where f is the \
-            field')
-    #parser.add_argument('-c','--create',action='store_true',
-            #help='Aspect ratio of figure: Height/Lenth',default=False)
-    args = parser.parse_args()
+  class myFormatter(argparse.RawDescriptionHelpFormatter,
+          argparse.ArgumentDefaultsHelpFormatter):
+          pass
+  parser = argparse.ArgumentParser(description=main.__doc__,
+          formatter_class=myFormatter)
+  parser.add_argument('DataFilePath',
+          help='Path to input file, restart or vtk')
+  parser.add_argument('Problem',
+          help='Name of the problem to plot: kedgeTop/freeSurfTop')
+  parser.add_argument('Method',
+          help='Method used for DNS: FD/Spectral')
+  parser.add_argument('Fields',
+          help='List of fields to plot')
+  parser.add_argument('Clips',type=list,
+          help='List of clipping values for each field')
+  parser.add_argument('-s','--size',dest='figBaseSize', type=int,
+          help='Base size of the figure',default=4)
+  parser.add_argument('-g','--gamma-disable',dest='gammaOpt',action='store_false',
+          help='Aspect ratio of figure: Height/Lenth',default=True)
+  parser.add_argument('-m','--max',dest='gma', type=float,
+          help='Global absolute maximum. gma = max(abs(f)) where f is the \
+          field')
+  #parser.add_argument('-c','--create',action='store_true',
+          #help='Aspect ratio of figure: Height/Lenth',default=False)
+  args = parser.parse_args()
 
-    fields = args.Fields.split(',')
-    clips = ''.join(args.Clips).split(',')
-    clips = list(map(float, clips))
+  fields = args.Fields.split(',')
+  clips = ''.join(args.Clips).split(',')
+  clips = list(map(float, clips))
 
-    return args.DataFilePath,fields,clips,\
-        args.figBaseSize,args.gammaOpt,args.gma
+  return args.DataFilePath,args.Problem,args.Method,fields,clips,\
+      args.figBaseSize,args.gammaOpt,args.gma
 
 def get_files_to_plot(DataFilePath):
-    drecs = []
-    if '*' in DataFilePath:
-        drecs = glob(DataFilePath) # list of input restarts, path included
-        if not drecs:
-            print('\nNO FILES FOUND MATCHING THE PATTERN:  ', DataFilePath)
-            print('--> QUITTING')
-            sys.exit(1)
-    else:
-        drecs.append(DataFilePath)
-        if not os.path.exists(DataFilePath):
-            print('\nFILE ',DataFilePath,' NOT FOUND')
-            print('--> QUITTING')
-            sys.exit(1)
-    drecs.sort()
-    return drecs
+  drecs = []
+  if '*' in DataFilePath:
+    drecs = glob(DataFilePath) # list of input restarts, path included
+    if not drecs:
+      print('\nNO FILES FOUND MATCHING THE PATTERN:  ', DataFilePath)
+      print('--> QUITTING')
+      sys.exit(1)
+  else:
+    drecs.append(DataFilePath)
+    if not os.path.exists(DataFilePath):
+      print('\nFILE ',DataFilePath,' NOT FOUND')
+      print('--> QUITTING')
+      sys.exit(1)
+  drecs.sort()
+  return drecs
 
 def mycf(X,Y,field,clip,fn=1,fb=4,fgamma=1.0,Nell=33,Nred=3,gma=None):
-    if not gma: # Calculate abolute maximum if gma not specified
-        gma = abs(field).max()
-    ell = clip*pylab.linspace(-1,1,Nell)*gma
-    red = pylab.linspace(clip*gma,gma,Nred)
-    blu = -red[::-1]
-    f,a = no_ax_fig(fn,fb,fgamma)
-    mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=blu,cmap=myBlues)
-    mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=ell,cmap=mycm15)
-    mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=red,cmap=myReds)
-    #if field.min() < 0 and field.max() > 0:
-    #    contour(X,Y,field,levels=[0],linestyles='-',colors='#777777')
-    return None
+  if not gma: # Calculate abolute maximum if gma not specified
+    gma = abs(field).max()
+  ell = clip*pylab.linspace(-1,1,Nell)*gma
+  red = pylab.linspace(clip*gma,gma,Nred)
+  blu = -red[::-1]
+  f,a = no_ax_fig(fn,fb,fgamma)
+  mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=blu,cmap=myBlues)
+  mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=ell,cmap=mycm15)
+  mycontourf(X,Y,field,lc='#777777',lw=0.1,levels=red,cmap=myReds)
+  #if field.min() < 0 and field.max() > 0:
+  #    contour(X,Y,field,levels=[0],linestyles='-',colors='#777777')
+  return None
 
 def main(f,fields,clips,fb,gammaOpt,gma):
     '''
@@ -103,9 +107,10 @@ def main(f,fields,clips,fb,gammaOpt,gma):
     return None
 
 if __name__ == '__main__':
-    DataFilePath,fields,clips,figBaseSize,gammaOpt,gma = get_args()
+  DataFilePath, probName, method, fields, clips, \
+   figBaseSize, gammaOpt, gma = get_args()
 
-#    if create: os.system("./myParaview < pv_input &")
+#  if create: os.system("./myParaview < pv_input &")
 
     if len(fields) is not len(clips):
         print('\nThe lists Fields and Clips must have the same lenth.')
@@ -113,16 +118,16 @@ if __name__ == '__main__':
         sys.exit(1)
 
 
-    drecs = get_files_to_plot(DataFilePath)
+  drecs = get_files_to_plot(DataFilePath)
 
-    NPROCS = 4
-    if NPROCS > 1:
-      mkl_set_num_threads(NPROCS)
-      pool = mp.Pool(processes=NPROCS)
-      D  = partial(main,fields=fields,clips=clips,
-              fb=figBaseSize,gammaOpt=gammaOpt,gma=gma)
-      DD = pool.map(D,drecs)
-    else:
-      for drec in drecs:
-        main(drec)
+  NPROCS = 4
+  if NPROCS > 1:
+    mkl_set_num_threads(NPROCS)
+    pool = mp.Pool(processes=NPROCS)
+    D  = partial(main,fields=fields,clips=clips,
+            fb=figBaseSize,gammaOpt=gammaOpt,gma=gma)
+    DD = pool.map(D,drecs)
+  else:
+    for drec in drecs:
+      main(drec)
 
